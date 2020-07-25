077ada72d63da42ad4ed98c4d2e8ecd87cab813e
Umesh <umesh.khunt@gmail.com>
2020-07-25 06:58:19 -0700

    static

--- src/frequentlyasked/PrimeCheck.java
+++ src/frequentlyasked/PrimeCheck.java
@@ -4,14 +4,12 @@ public class PrimeCheck {
 	
 	public static void main (String[] arg){
 		
-		PrimeCheck primechk = new PrimeCheck(); // Becoz isPrime check is not static so creating instance first.
-		
 		int n = 13;
-		System.out.println("The Number " + n + " is prime number: " + primechk.isPrime(n));		
+		System.out.println("The Number " + n + " is prime number: " + isPrime(n));		
 	}
 
 	
-	 public boolean isPrime(int n){
+	 public static boolean isPrime(int n){
 		 if (n < 2) return false; // 0 and 1 are not prime, but 2 is prime 
 		 
 		 for(int i = 2; i < n; i ++){
